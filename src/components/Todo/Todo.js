a06a4906fcbef558bb5479b3141aabedf2a7065c
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-23 21:20:27 +0100

    integrate Todo component

---
+++ src/components/Todo/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import TodoAdd from './TodoAdd'
+import TodoContent from './TodoContent'
+
+function Todo() {
+    return (
+        <>
+        <TodoAdd />
+
+        <hr />
+
+        <TodoContent />
+
+        <hr />
+        </>
+    )
+}
+
+export default Todo
