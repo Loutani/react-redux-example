b80a0c8400be092b8902a34d558eb4c8f1d6427f
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-23 21:41:30 +0100

    add state to handle the todo text content

--- src/components/Todo/TodoAdd.js
+++ src/components/Todo/TodoAdd.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function TodoAdd() {
+    const [todo, setTodo] = useState('')
+
+    const handleChange = (e) => {
+        setTodo(e.target.value)
+    }
     return (
         <div className="add-todo">
-            <input />
+            <input type='text' value={todo} onChange={handleChange} />
             <button>Add todo</button>
         </div>
     )
