409ac4c03165be15970092f0ecc5a53e623d7634
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-23 21:39:15 +0100

    integrate the get todos from store

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,15 +1,22 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import TodoAdd from './TodoAdd'
 import TodoContent from './TodoContent'
 
 function Todo() {
+    const todos = useSelector(state => state.todos)
     return (
         <>
         <TodoAdd />
 
         <hr />
 
-        <TodoContent />
+        {
+            todos.length > 0 && todos.map(todo => <TodoContent key={todo.id} todo={todo} />)
+        }
+        {
+            todos.length <= 0 && <p>no todo found</p>
+        }
 
         <hr />
         </>
