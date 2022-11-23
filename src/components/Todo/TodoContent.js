cbaa56495b446b06c5509c826d7ca67a539e1460
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-23 21:20:18 +0100

    integrate TodoContent component

---
+++ src/components/Todo/TodoContent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TodoContent() {
+    return (
+        <div className="todos">
+            <div className="todo">
+                <p>todo content</p>
+                <button>toggle</button>
+                <button>delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default TodoContent
