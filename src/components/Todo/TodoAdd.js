5f303909d998159b454fdb799c1657a8cac1a3b3
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-23 21:44:29 +0100

    integrate the create new todo dispatch

--- src/components/Todo/TodoAdd.js
+++ src/components/Todo/TodoAdd.js
@@ -1,15 +1,32 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addTodo } from '../../redux/slices/TodoSlice';
 
 function TodoAdd() {
     const [todo, setTodo] = useState('')
 
+    const dispatch = useDispatch();
+
     const handleChange = (e) => {
         setTodo(e.target.value)
     }
+
+    const handleAddTodo = () => {
+        const newTodo = {
+            id: Math.random,
+            content: todo,
+            completed: false
+        }
+
+        dispatch(addTodo(newTodo));
+
+        setTodo('');
+    }
+
     return (
         <div className="add-todo">
             <input type='text' value={todo} onChange={handleChange} />
-            <button>Add todo</button>
+            <button onClick={handleAddTodo}>Add todo</button>
         </div>
     )
 }
