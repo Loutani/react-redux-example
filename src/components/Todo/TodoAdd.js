314671a03c02d2c8b788b3f0c455c40c8d28f765
Loutani mohammed es-salih <lotchomimo16saleh@gmail.com>
2022-11-23 21:19:57 +0100

    integrate TodoAdd component

---
+++ src/components/Todo/TodoAdd.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TodoAdd() {
+    return (
+        <div className="add-todo">
+            <input />
+            <button>Add todo</button>
+        </div>
+    )
+}
+
+export default TodoAdd
